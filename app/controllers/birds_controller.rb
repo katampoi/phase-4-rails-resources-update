310a30a8dddec5a9b710e78c1375f34c437a1eda
katampoi <simonkatampoi@student.moringaschool.com>
2022-09-28 23:09:23 +0300

    Complete Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,23 @@ class BirdsController < ApplicationController
     end
   end
 
+    # PATCH /birds/:id
+    def update
+      # find_bird
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        # update_bird
+        bird.update(bird_params)
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
